869b2accadf1215314d7390ca0cfd25a2ff53c43
raulprietoUR <raulprietog98@gmail.com>
2023-03-06 23:23:40 +0100

    a

--- app/src/test/java/com/example/android/unscramble/ui/test/GameViewModelTest.kt
+++ app/src/test/java/com/example/android/unscramble/ui/test/GameViewModelTest.kt
@@ -31,25 +31,13 @@ class GameViewModelTest {
 
     @Test
     fun gameViewModel_Initialization_FirstWordLoaded() {
-        /**
-         *  Warning: This way to retrieve the uiState works because MutableStateFlow is used. In the
-         *  upcoming units you will learn about advanced usages of StateFlow that creates a stream
-         *  of data and you need to react to handle the stream. For those scenarios you will write
-         *  unit tests using different methods/approaches. This applies to all the usages of
-         *  viewModel.uiState.value in this class.
-         **/
         val gameUiState = viewModel.uiState.value
         val unScrambledWord = getUnscrambledWord(gameUiState.currentScrambledWord)
 
-        // Assert that current word is scrambled.
         assertNotEquals(unScrambledWord, gameUiState.currentScrambledWord)
-        // Assert that current word count is set to 1.
         assertTrue(gameUiState.currentWordCount == 1)
-        // Assert that initially the score is 0.
         assertTrue(gameUiState.score == 0)
-        // Assert that wrong word guessed is false.
         assertFalse(gameUiState.isGuessedWordWrong)
-        // Assert that game is not over.
         assertFalse(gameUiState.isGameOver)
     }
 
